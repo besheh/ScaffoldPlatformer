8e6bcb375857375c7517c6ce1ba96ae0a22cdc03
Bryan Ly <bryanly200@gmail.com>
2019-02-13 15:27:08 -0800

    Update Timer.cs
    Now restarts scene when timer hits 0

--- Platformer/Assets/Scripts/Timer.cs
+++ Platformer/Assets/Scripts/Timer.cs
@@ -2,10 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 // Add script component to Main Camera
 // Bind a Canvas -> Text onto Time Text box
-
+/*
 public class Timer : MonoBehaviour
 {
 
@@ -16,7 +17,7 @@ public class Timer : MonoBehaviour
     void Start()
     {
         // Time limit for the level
-        TimeStamp = 100;
+        TimeStamp = 5;
     }
 
     // Update is called once per frame
@@ -36,7 +37,40 @@ public class Timer : MonoBehaviour
     // Called when the timer reaches 0
     public void FinishAction()
     {
-        Debug.Log("Time's Up");
-        TimeText.text = "00:00";
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
+*/
+public class Timer : MonoBehaviour
+{
+
+    public Text TimeText;
+    public float TimeStamp;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Time limit for the level
+        TimeStamp = 30;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float t = TimeStamp - Time.timeSinceLevelLoad;
+        if (t <= 0)
+        {
+            FinishAction();
+            return;
+        }
+        string minutes = ((int)t / 60).ToString("00");
+        string seconds = (t % 60).ToString("00");
+        TimeText.text = minutes + ":" + seconds;
+    }
+
+    // Called when the timer reaches 0
+    public void FinishAction()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
